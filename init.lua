aa1140ca778e4be2640ccda4f3bd70b1d5640d36
Lucas Frykman <lucas.frykman@tink.com>
2024-08-30 14:38:56 +0200

    update: updated init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=2") vim.cmd("set softtabstop=2") vim.cmd("set shiftwidth=2")
 
 -- Enable line numbers by default
 vim.opt.number = true
@@ -30,9 +28,7 @@ vim.opt.laststatus = 2
 -- vim.cmd('colorscheme elflord')
 -- vim.cmd('colorscheme evening')
 -- vim.cmd('colorscheme industry')
--- vim.cmd('colorscheme desert')
--- vim.cmd('colorscheme desert')
-
+--
 -- Remove M characters 
 vim.cmd [[command! RemoveM %s/\r//g]]
 
@@ -48,7 +44,11 @@ vim.cmd([[
 ]])
 
 -- Map Escape in terminal to exit terminal mode
-vim.cmd('tnoremap <Esc> <C-\\><C-n>')
+-- vim.cmd('tnoremap <Esc> <C-\\><C-n>')
+-- high
+-- vim.api.nvim_exec([[
+--   highlight Type ctermfg=Yellow guifg=#FFFF00
+-- ]], false)
 
 -- Auto change directory to current file's directory when opening a buffer
 vim.cmd([[
@@ -56,7 +56,7 @@ vim.cmd([[
 ]])
 
 -- Define a custom command to split and open terminal
-vim.cmd [[command! SplitTerm split | terminal | startinsert]]
+-- vim.cmd [[command! SplitTerm split | terminal | startinsert]]
 vim.cmd[[
   command! SplitTerm call StartSplitTerm()
   function! StartSplitTerm()
@@ -68,33 +68,90 @@ vim.cmd[[
 
 -- Load plugins using Packer
 require('packer').startup(function()
-  -- Packer can manage itself
+
   use 'wbthomason/packer.nvim'
+
+  use 'tanvirtin/monokai.nvim'
+
+
   use 'tpope/vim-commentary'
+
   use 'b3nj5m1n/kommentary'    
+
   use 'preservim/nerdtree'
-  -- use 'tanvirtin/monokai.nvim'
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }}
-  -- install without yarn or npm
-  -- use({
-  --   "iamcco/markdown-preview.nvim",
-  --   run = function() vim.fn["mkdp#util#install"]() end,
-  -- })
+
+  use 'tpope/vim-fugitive'
+
+  use {'nvim-lualine/lualine.nvim', requires = {'nvim-tree/nvim-web-devicons', opt = true }}
 
   -- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 end)
 
-local custom_gruvbox = require'lualine.themes.gruvbox'
+
+-- Just monokai stuff
+require('monokai').setup {}
+require('monokai').setup { palette = require('monokai').pro }
+-- require('monokai').setup { palette = require('monokai').soda }
+-- require('monokai').setup { palette = require('monokai').ristretto }
 
 -- Change the background of lualine_c section for normal mode
+local custom_gruvbox = require'lualine.themes.gruvbox'
+
 custom_gruvbox.normal.c.bg = '#112233'
 
 require('lualine').setup {
   options = { theme  = custom_gruvbox },
 }
--- require('monokai').setup {}
--- require('monokai').setup { palette = require('monokai').pro }
--- require('monokai').setup { palette = require('monokai').soda }
--- require('monokai').setup { palette = require('monokai').ristretto }
+
+
+
+-- vim.api.nvim_exec([[
+--   " Define a function to add syntax highlighting for a language
+--   function! MySyntaxHighlighting()
+--     if &filetype == 'python'
+--       " Python
+--       syntax match pythonType "\\<class\\>"
+--       highlight link pythonType Type
+--     elseif &filetype == 'go'
+--       " Go
+--       syntax match goType "\\<type\\>"
+--       highlight link goType Type
+--     elseif &filetype == 'java'
+--       " Java
+--       syntax match javaType "\\<class\\|interface\\>"
+--       highlight link javaType Type
+--     endif
+--   endfunction
+
+--   " Call the function when a buffer is read or a new buffer is created
+--   autocmd BufRead,BufNewFile * call MySyntaxHighlighting()
+
+--   " Set the color for Type
+--   highlight Type ctermfg=LightGreen guifg=#90EE90
+-- ]], false)
+
+vim.api.nvim_exec([[
+  " Define a function to add syntax highlighting for a language
+  function! MySyntaxHighlighting()
+    if &filetype == 'python'
+      " Python
+      syntax match pythonType "\\<class\\s\\+\\w\\+\\>"
+      highlight link pythonType Type
+    elseif &filetype == 'go'
+      " Go
+      syntax match goType "\\<type\\s\\+\\w\\+\\>"
+      highlight link goType Type
+    elseif &filetype == 'java'
+      " Java
+      syntax match javaType "\\<class\\|interface\\s\\+\\w\\+\\>"
+      highlight link javaType Type
+    endif
+  endfunction
+
+  " Call the function when a buffer is read or a new buffer is created
+  autocmd BufRead,BufNewFile * call MySyntaxHighlighting()
+
+  " Set the color for Type
+  highlight Type ctermfg=LightGreen guifg=#90EE90
+]], false)
